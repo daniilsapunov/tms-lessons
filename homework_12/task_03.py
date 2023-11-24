7041096dfd2799ea6615a2e03a87b1bc68bb2c66
daniil Sapunov <daniilsapunov133@gmail.com>
2023-11-24 10:46:52 +0300

    update

--- homework_12/task_03.py
+++ homework_12/task_03.py
@@ -2,11 +2,13 @@ import re
 
 
 def is_float_number(string: str) -> bool:
-    return re.fullmatch(r'\+?\-?\d+\.\d+', string) is not None
+    return re.fullmatch(r'[-+]?[0-9]*\.?[0-9]+', string) is not None
+
+print(is_float_number('+-12.34'))
 
 if __name__ == '__main__':
     assert is_float_number('1231231.12313123')
-    assert is_float_number('+1231231.12313123')
+    assert is_float_number('+1231.12323')
     assert is_float_number('-1231231.12313123')
     assert not is_float_number('o231231.1123')
     assert not is_float_number('--1231231.12313123')
